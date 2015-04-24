0067e833eeddf4ed1c4bb1fa23ef94087bb6233c
Yumna Aziz <yumna_ansari@hotmail.com>
2015-04-24 19:16:03 +0300

    Update

---
+++ Assets/_Scripts/selectPlant.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectPlant : MonoBehaviour {
+	private bool currentSelected;
+
+	private void OnMouseDown(){
+		if (GameManager.instance.plantSelected == false && currentSelected == false && 
+		    GameManager.instance.tileSelected == true && GameManager.instance.getResult()>0) {
+
+			GameManager.instance.plantSelected = true;
+			currentSelected = true;
+			GameManager.instance.setPlantType(gameObject.name);
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, .5f);
+			Debug.Log (gameObject.name);
+
+		} else if(GameManager.instance.plantSelected == true && currentSelected == true)
+		{
+			Debug.Log (gameObject.name);
+			GameManager.instance.plantSelected = false;
+			currentSelected = false;
+			GameManager.instance.setPlantType(null);
+			GetComponent<SpriteRenderer>().color = new Color (1f, 1f, 1f, 1f);
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+		currentSelected = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GameManager.instance.tileSelected == false) {
+			GetComponent<SpriteRenderer>().color = new Color (1f, 1f, 1f, 1f);
+			currentSelected = false; 
+			GameManager.instance.setPlantType(null);
+			GameManager.instance.plantSelected = false;
+		}
+	
+	}
+}
