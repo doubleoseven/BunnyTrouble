3051d6286c8607b89f998cfb1ba4fe4a6a8e521d
SondosAzz <sondos.azzouz@gmail.com>
2015-04-27 18:02:34 +0300

    ...

--- Assets/_Scripts/enemyMovement.cs
+++ Assets/_Scripts/enemyMovement.cs
@@ -2,23 +2,14 @@
 using System.Collections;
 
 public class enemyMovement : MonoBehaviour {
-	CharacterController controller;
-	public float speed = .02f;
-	 bool dir1= true;
+//CharacterController controller;
+	public float speed ;
 
 
 
-	void Start () 
-	{
-		controller = GetComponent<CharacterController>();
-	
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if (dir1) 
-		{
-			transform.Translate(-Vector2.right*speed*Time.deltaTime);
-		}
+		transform.Translate(-Vector2.right*speed*(Time.deltaTime));
+
 	}
 }
