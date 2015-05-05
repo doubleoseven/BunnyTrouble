a211caf23a7a9f877fd85307f2f3242342e82fae
Yumna Aziz <yumna_ansari@hotmail.com>
2015-05-05 13:14:31 +0300

    Added a bunniesToSave on screen

--- Assets/_Scripts/winScript.cs
+++ Assets/_Scripts/winScript.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class winScript : MonoBehaviour {
 
 	public int bunniesToSave = 12;
+	public int bunniesLeft; 
+	Text text;  
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		text = GetComponent <Text> ();
+		bunniesLeft = 12;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		bunniesLeft = bunniesToSave - GameManager.instance.bunniesSaved;
+		text.text = "Bunnies Left: " + bunniesLeft;
 		if (GameManager.instance.bunniesSaved == bunniesToSave) {
 			Application.LoadLevel(6);
 		}
